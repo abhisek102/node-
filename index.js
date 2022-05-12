6c6b356455be0b8b007f1f1b633c08455d4ba34b
abhisek102 <abhisek.nanda@contractor.tatacommunications.com>
2022-05-12 19:30:48 +0530

    1 file modified

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const arr = {
 
 const datajson = JSON.stringify(arr);
 console.log(datajson);
+
+const objdata = JSON.parse(datajson);
+console.log(objdata);
+console.log(objdata.firstname);
+console.log(objdata.lastname);
+console.log(objdata.age);
+console.log(objdata.intrest);
