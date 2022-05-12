a7e115aa0c413dc53736e2d14ac9205a20f3ea31
abhisek102 <abhisek.nanda@contractor.tatacommunications.com>
2022-05-12 19:19:53 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const arr= {
-  firstname='Abhisek',
-  lastname='Nanda',
-  age='22',
-  intrest='girls'
+const arr = {
+  firstname: 'Abhisek',
+  lastname: 'Nanda',
+  age: '22',
+  intrest: 'girls',
 };
 
-const datajson= JSON.stringify(arr);
+const datajson = JSON.stringify(arr);
 console.log(datajson);
